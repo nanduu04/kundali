2e2092f4c5ce812b208edc8fd6a694f5e22624be
Nandu Pokhrel <nandupokhrel@gmail.com>
2025-05-18 12:12:09 -0400

    build 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,53 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true,
+    jest: true
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
     'react',
     'react-hooks',
+    'jsx-a11y'
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'no-unused-vars': ['warn', { 
       'argsIgnorePattern': '^_',
-      'varsIgnorePattern': '^_'
+      'varsIgnorePattern': '^_',
+      'ignoreRestSiblings': true
     }],
     'react/no-unescaped-entities': 'off',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.test.jsx'],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }; 
